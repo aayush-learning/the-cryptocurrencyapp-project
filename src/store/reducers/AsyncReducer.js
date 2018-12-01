7f11198449524478210b5fe9ffed9390567dc394
Karan Pratap Singh <vertigo101@Karans-MacBook-Air.local>
2018-12-01 20:54:21 +0530

    changes:  Published

--- src/store/reducers/AsyncReducer.js
+++ src/store/reducers/AsyncReducer.js
@@ -37,7 +37,7 @@ const asyncReducer = (state = initialState, action) => {
 
                 ...initialState
             };
-            
+        
         default:
             return state;
     }
